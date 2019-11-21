dd8e642ab5e10fbceb3ef5337cdeb718ac4739e7
Josep Vidal <josepvidalvidal@gmail.com>
2019-11-21 22:27:29 +0100

    Updates

---
+++ precache-manifest.c4d2461dc7337e2e29c7481c2132effd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "656101aedba47e216fbd7d20f08267c7",
+    "url": "/vblogv/index.html"
+  },
+  {
+    "revision": "04974d03da4edadb0272",
+    "url": "/vblogv/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "2e6f01c36a1ec3d3eb90",
+    "url": "/vblogv/static/css/main.b129782f.chunk.css"
+  },
+  {
+    "revision": "04974d03da4edadb0272",
+    "url": "/vblogv/static/js/2.1ccba288.chunk.js"
+  },
+  {
+    "revision": "2e6f01c36a1ec3d3eb90",
+    "url": "/vblogv/static/js/main.7abc0f47.chunk.js"
+  },
+  {
+    "revision": "2bae2d64d5c83fb11877",
+    "url": "/vblogv/static/js/runtime-main.c14aed14.js"
+  },
+  {
+    "revision": "c958a80d8e78e49405743438ea6cd8e8",
+    "url": "/vblogv/static/media/me-finland.c958a80d.jpg"
+  },
+  {
+    "revision": "19d9e0dd62c886a964f5df35c1f42ab6",
+    "url": "/vblogv/static/media/me-roman.19d9e0dd.png"
+  },
+  {
+    "revision": "98a849623fa3dc158d58a09e1210cd79",
+    "url": "/vblogv/static/media/square-placeholder.98a84962.png"
+  }
+]);
