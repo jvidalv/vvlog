e3605b60cd1069f4c14ba37a55937a5575ffe6cf
Josep Vidal <josepvidalvidal@gmail.com>
2020-01-23 23:21:49 +0100

    Updates

---
+++ precache-manifest.e1f4ec2652d8e933a5b0f0d684452081.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1df86b1293740578bdd0e838bd835fc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e253fda417862be7802",
+    "url": "/static/css/2.85244a98.chunk.css"
+  },
+  {
+    "revision": "2cce1a11d20474d491dc",
+    "url": "/static/css/main.645fd4cf.chunk.css"
+  },
+  {
+    "revision": "8e253fda417862be7802",
+    "url": "/static/js/2.bd9e0dcc.chunk.js"
+  },
+  {
+    "revision": "953e12b376508e384bbb764a199bd423",
+    "url": "/static/js/2.bd9e0dcc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2cce1a11d20474d491dc",
+    "url": "/static/js/main.c334b547.chunk.js"
+  },
+  {
+    "revision": "060c69b706fb0aa49284",
+    "url": "/static/js/runtime-main.e10d3098.js"
+  },
+  {
+    "revision": "c958a80d8e78e49405743438ea6cd8e8",
+    "url": "/static/media/me-finland.c958a80d.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vvlog/precache-manifest.8f2d3ba05476198b8c4ded3e015a5c88.js"
+  "/precache-manifest.e1f4ec2652d8e933a5b0f0d684452081.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vvlog/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
