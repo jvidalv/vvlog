359c152129b2ca191617efa44c349c9018b74b7c
Josep Vidal <josepvidalvidal@gmail.com>
2020-04-08 18:33:43 +0200

    📕 Fixed typo

--- src/components/Loaders.js
+++ src/components/Loaders.js
@@ -14,7 +14,7 @@ const useIsHome = () => {
 /**
  * @returns {*}
  * @constructor
- * todo its better to split in diferent components and check for home path in other place
+ * todo its better to split in different components and check for home path in other place
  */
 export function MainLoader(props) {
 
